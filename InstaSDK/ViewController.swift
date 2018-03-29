f04961e3cdf9f3989417ea180393c2f204fd6363
Nazar Kuradovets <Nazar@macbook-air-liliana.local>
2018-03-29 20:33:42 +0300

    Initial Commit

---
+++ InstaSDK/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaSDK
+//
+//  Created by Nazar Kuradovets on 29.03.18.
+//  Copyright © 2018 Nazar_Kuradovets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
